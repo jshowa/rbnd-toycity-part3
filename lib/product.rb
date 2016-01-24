4fe5e53714a58f08c18304c591a7eb6cf50098f1
Jacob Howarth <jshowa.git@gmail.com>
2016-01-23 23:54:47 -0500

    Added product find by title, accessors for price and stock, in stock check method and in stock class method that finds all products with stock > 0.

--- lib/product.rb
+++ lib/product.rb
@@ -5,7 +5,7 @@ class Product
   @@products = []
 
   def add_product
-    if (@@products.select { |product| product.title == @title }.length) > 0
+    if Product.find_by_title(@title) != nil
       raise DuplicateProductError, "'#{@title}' already exists."
     end
     @@products << self
@@ -13,13 +13,25 @@ class Product
 
   public
 
-  attr_reader :title
+  attr_reader :title, :price, :stock
   
   def initialize(arguments={})
     @title = arguments[:title] || ""
     @price = arguments[:price] || 0.0
     @stock = arguments[:stock] || 0
     add_product
+  end
+
+  def in_stock?
+    (@stock > 0)
+  end
+  
+  def self.in_stock
+    @@products.select { |product| product.stock > 0 }
+  end
+
+  def self.find_by_title(target = "")
+    @@products.select { |product| product.title == target }[0]
   end
 
   def self.all
